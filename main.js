00538e8c30ac5a717812af2e03e525eb10404622
Taylor Cox <taylorashtoncox@gmail.com>
2017-07-18 13:39:20 -0400

    Fixed fetch

--- main.js
+++ main.js
@@ -1,18 +1,19 @@
 const recipeContainer = document.querySelector(".recipes")
-const title = document.createElement("h2")
-const image = document.createElement("img")
 const recipeList = document.querySelector(".recipe-list")
-let searchterm = 'schnauzer'
+let searchterm = 'omelette'
 
-const promise = fetch(`http://www.recipepuppy.com/api/?q=${searchterm}`, {mode: 'no-cors'})
+const promise = fetch(`https://crossorigin.me/http://www.recipepuppy.com/api/?q=${searchterm}`)
   .then(response => response.json())
   .then(recipes => {
     console.log(recipes);
+  recipes.results.forEach(function (recipeData, index) {
+    const indRecipe = document.createElement("li")
+    const title = document.createElement("h2")
+    const image = document.createElement("img")
+    title.textContent = recipeData.title
+    image.src = recipeData.thumbnail
+    recipeList.appendChild(indRecipe)
+    indRecipe.appendChild(image)
+    indRecipe.appendChild(title)
   })
-  // recipes.forEach(function (recipe, index) {
-  //   const indRecipe = document.createElement("li")
-  //   title.textContent = recipes.title
-  //   image.src = recipes.thumbnail
-  //   recipeList.appendChild(indRecipe)
-  //   indRecipe.appendChild(img)
-  //   indRecipe.appendChild(title)
+})
