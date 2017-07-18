514f8b8c2208e0859d8cf4aa0e33347cdd4462c4
Taylor Cox <taylorashtoncox@gmail.com>
2017-07-18 14:28:39 -0400

    Got search input working

--- main.js
+++ main.js
@@ -1,19 +1,31 @@
 const recipeContainer = document.querySelector(".recipes")
 const recipeList = document.querySelector(".recipe-list")
-let searchterm = 'omelette'
+const button = document.querySelector("button")
 
-const promise = fetch(`https://crossorigin.me/http://www.recipepuppy.com/api/?q=${searchterm}`)
-  .then(response => response.json())
-  .then(recipes => {
-    console.log(recipes);
-  recipes.results.forEach(function (recipeData, index) {
-    const indRecipe = document.createElement("li")
-    const title = document.createElement("h2")
-    const image = document.createElement("img")
-    title.textContent = recipeData.title
-    image.src = recipeData.thumbnail
-    recipeList.appendChild(indRecipe)
-    indRecipe.appendChild(image)
-    indRecipe.appendChild(title)
+
+  button.addEventListener("click", searchEvent => {
+    let searchTerm = document.querySelector("#search").value
+    const promise = fetch(`https://crossorigin.me/http://www.recipepuppy.com/api/?q=${searchTerm}`)
+      .then(response => response.json())
+      .then(recipes => {
+        console.log(recipes)
+        recipeList.innerHTML = ""
+      recipes.results.forEach(function (recipeData, index) {
+        const indRecipe = document.createElement("li")
+        const title = document.createElement("h2")
+        const image = document.createElement("img")
+        const link = document.createElement("a")
+        link.href = recipeData.href
+        title.textContent = recipeData.title
+        if (recipeData.thumbnail) {
+          image.src = recipeData.thumbnail
+        } else {
+          image.src = "http://via.placeholder.com/172x130"
+        }
+        recipeList.appendChild(indRecipe)
+        link.appendChild(image)
+        link.appendChild(title)
+        indRecipe.appendChild(link)
+      })
+    })
   })
-})
