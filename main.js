7c2535b03a30b773557ed2b8a88eae26a4318d5e
Taylor Cox <taylorashtoncox@gmail.com>
2017-07-18 13:13:19 -0400

    API not connecting, waiting on instructor help, but updated HTML to include unordered list, set up forEach to pull in the title and images for the recipes and set those each to be a list item, and added CSS styling to flex each recipe in four columns

--- main.js
+++ main.js
@@ -1,13 +1,18 @@
 const recipeContainer = document.querySelector(".recipes")
 const title = document.createElement("h2")
 const image = document.createElement("img")
+const recipeList = document.querySelector(".recipe-list")
+let searchterm = 'schnauzer'
 
-const promise = fetch("http://www.recipepuppy.com/api/")
+const promise = fetch(`http://www.recipepuppy.com/api/?q=${searchterm}`, {mode: 'no-cors'})
   .then(response => response.json())
   .then(recipes => {
     console.log(recipes);
   })
-  // recipes.results.forEach(function (person, index) {
-  //   const characterInd = document.createElement("li")
-  //   characterInd.textContent = person.name
-  //   characters.appendChild(characterInd)
+  // recipes.forEach(function (recipe, index) {
+  //   const indRecipe = document.createElement("li")
+  //   title.textContent = recipes.title
+  //   image.src = recipes.thumbnail
+  //   recipeList.appendChild(indRecipe)
+  //   indRecipe.appendChild(img)
+  //   indRecipe.appendChild(title)
